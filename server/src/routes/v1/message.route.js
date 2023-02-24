dd3ef2e417e73ca1953deff44956a63ba63093ea
VuThanhSang <zimb240@gmail.com>
2023-02-24 22:00:49 +0700

    api message

--- server/src/routes/v1/message.route.js
+++ server/src/routes/v1/message.route.js
@@ -2,9 +2,32 @@ const express = require("express");
 const verifyToken = require("../../middlewares/verifyToken");
 const router = express.Router();
 const messageController = require("../../controllers/message.controller");
+const videoFileUploader = require("../../middlewares/videocloudinary");
+const imageFileUploader = require("../../middlewares/imagecloudinary");
 
 router.route("/sendMessage").post(verifyToken, messageController.sendMessage);
+router
+  .route("/sendImage")
+  .post(
+    verifyToken,
+    imageFileUploader.array("files"),
+    messageController.sendMessage
+  );
+router
+  .route("/sendVideo")
+  .post(
+    verifyToken,
+    videoFileUploader.array("files"),
+    messageController.sendMessage
+  );
 
+router
+  .route("/removeSource/:id")
+  .put(verifyToken, messageController.removeSource);
+
+router
+  .route("/deleteMessage/:id")
+  .put(verifyToken, messageController.removeSource);
 router
   .route("/editMessage/:id")
   .put(verifyToken, messageController.editMessage);
--- server/src/services/message.service.js
+++ server/src/services/message.service.js
@@ -1,9 +1,58 @@
 const messageModel = require("../models/message.model");
-
-const sendMessage = async (data) => {
+require("dotenv");
+const cloudinary = require("cloudinary").v2;
+const sendMessage = async (req) => {
   try {
-    const result = await messageModel.sendMessage(data);
+    const source = req.files.map((data) => {
+      return {
+        data: data.path,
+        type: data.mimetype,
+        filename: data.filename,
+      };
+    });
+    const addData = { ...req.body, source, sourceId: req.user.sub };
+    const result = await messageModel.sendMessage(addData);
     return result;
+  } catch (error) {
+    if (req.files) {
+      req.files.map((item) => {
+        cloudinary.uploader.destroy(item.filename);
+      });
+    }
+    throw new Error(error);
+  }
+};
+
+const removeSource = async (id, userId) => {
+  try {
+    const message = await messageModel.findOneById(id);
+    if (message.sourceId === userId) {
+      const update = { ...message, source: [] };
+      await messageModel.editMessage(update, id);
+      message.source.map((item) => {
+        cloudinary.uploader.destroy(item.filename);
+      });
+      return { status: true, message: "successfully" };
+    } else {
+      return { status: false, message: "You are not authorized to do this" };
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const deleteMessage = async (id, userId) => {
+  try {
+    const message = await messageModel.findOneById(id);
+    if (message.sourceId === userId) {
+      await message.deleteMessage(id);
+      message.source.map((item) => {
+        cloudinary.uploader.destroy(item.filename);
+      });
+      return { status: true, message: "successfully" };
+    } else {
+      return { status: false, message: "You are not authorized to do this" };
+    }
   } catch (error) {
     throw new Error(error);
   }
@@ -41,4 +90,6 @@ module.exports = {
   editMessage,
   showDirectMessage,
   showChannelMessage,
+  removeSource,
+  deleteMessage,
 };
